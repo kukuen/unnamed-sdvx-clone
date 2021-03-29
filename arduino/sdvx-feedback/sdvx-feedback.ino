ad6c44a598877d0ce3fd98b4310d6881b8f000f0
kukuen <mister.qqn@gmail.com>
2021-03-30 00:03:56 +0200

    multiplexer working

--- arduino/sdvx-feedback/sdvx-feedback.ino
+++ arduino/sdvx-feedback/sdvx-feedback.ino
@@ -1,33 +1,64 @@
 
+#include <stdio.h>
 #include <Wire.h>
 #include "Adafruit_DRV2605.h"
 
 Adafruit_DRV2605 drv;
 
-void setup() {
+void selectBus(uint8_t bus)
+{
+  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+}
+
+void setup() {  
+  Wire.begin();  
   Serial.begin(9600);
-  Serial.println("DRV test");
+  Serial.println("Started");
+
+  selectBus(2);
+  initDrv();
+  
+  selectBus(3);
+  initDrv();    
+}
+
+void initDrv() {
   drv.begin();
   drv.useLRA(); 
-  
   drv.selectLibrary(6);
-  
-  // I2C trigger by sending 'go' command 
-  // default, internal trigger when sending GO command
   drv.setMode(DRV2605_MODE_INTTRIG); 
+}
+
+
+void loop() {
   
+  selectBus(2);
+  play();
+
+ delay(100);
+  
+  selectBus(3);
+  play();
+  
+ delay(100);
+}
+
+void play(){
+      drv.stop();
+      delayMicroseconds(1);
+      
       // set the effect to play
       drv.setWaveform(0, 1);  // play effect 
       drv.setWaveform(1, 0);       // end waveform
     
       // play the effect!
-      drv.go();
+      drv.go();  
 }
 
+void __loop() {
 
-void loop() {
-
-  
   while(Serial.available()){
     
     if(Serial.read()){
@@ -43,6 +74,5 @@ void loop() {
     }
    
   }
-  
-  delay(1);    
+  delay(100);    
 }
